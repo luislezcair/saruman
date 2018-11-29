726e22a32511774048b8edb882269034c6d3cca0
Juan Antonio Damian Vargas <juanantoniodamianv@gmail.com>
2018-11-29 13:54:05 -0300

    Add Relationship: Product, Producer and Category
    Product has and belongs to many with Category.
    Category has and belongs to many with Product.
    Producer has one Product.
    Product has one Producer.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+  extend Enumerize
+
+  has_and_belongs_to_many :categories
+  validates :product_number, :name, :type, presence :true
+
+  enumerize :type, in: [:accessory, :product, :service]
 end
