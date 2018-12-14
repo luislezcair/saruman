643b1a1f2a91d3ff858af184470687c92b668305
Juan Antonio Damian Vargas <juanantoniodamianv@gmail.com>
2018-12-14 09:58:26 -0300

    Add DBI Column To Products
    Add migration to generate dbi column on products.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_12_192643) do
+ActiveRecord::Schema.define(version: 2018_12_14_124459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -239,6 +239,7 @@ ActiveRecord::Schema.define(version: 2018_12_12_192643) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "producer_id"
+    t.string "dbi"
     t.index ["producer_id"], name: "index_products_on_producer_id"
   end
 
