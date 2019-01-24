80969117e443732d16d1365b1126aaf47ad68f46
Juan Antonio Damian Vargas <juanantoniodamianv@gmail.com>
2019-01-24 10:00:19 -0300

    [Updated]  Check If Exists Column Before Remove

--- db/migrate/20190124111330_remove_contry_from_address.rb
+++ db/migrate/20190124111330_remove_contry_from_address.rb
@@ -1,8 +1,14 @@
 class RemoveContryFromAddress < ActiveRecord::Migration[5.2]
   def change
-    remove_column :addresses, :city, :string
-    remove_column :addresses, :country, :string
-    remove_column :addresses, :state, :string
+    unless column_exists? :addresses, :city
+      add_column :addresses, :city, :string
+    end
+    unless column_exists? :addresses, :country
+      add_column :addresses, :country, :string
+    end
+    unless column_exists? :addresses, :state
+      add_column :addresses, :state, :string
+    end
     add_reference :addresses, :city, foreign_key: true
     add_reference :addresses, :province, foreign_key: true
     add_reference :addresses, :country, foreign_key: true
