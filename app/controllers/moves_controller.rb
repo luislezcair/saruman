9be23fbdfec8025c80d0a85090b430eaac92ab9c
Juan Antonio Damian Vargas <juanantoniodamianv@gmail.com>
2019-01-29 09:45:01 -0300

    Has Been Added Show View For Deposit Moves

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -1,14 +1,22 @@
 class MovesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_move, only: [:show, :edit, :update]
+  authorize_resource
+
   def index
     @moves = Move.joins(:user_take, :user_register, move_details: :site_to).distinct()
   end
 
   def edit
-    @move = Move.find_by_id(params[:id])
     @move_details = MoveDetail.joins(:inventory).where(move_id: params[:id])
   end
 
   def update
+    if @move.update(move_params)
+      redirect_to moves_path
+    else
+      render :edit, alert: :error
+    end
   end
 
   def show
@@ -18,7 +26,7 @@ class MovesController < ApplicationController
     @move_detail = MoveDetail.find(params[:id])
     move = @move_detail.move
     inventory = Inventory.find(@move_detail.inventory.id)
-    inventory.update_attributes(status: :disponible)
+    inventory.update_attributes(status: :disponible, deposit: @move_detail.site_from)
     @move_detail.destroy
 
     respond_to do |format|
@@ -26,5 +34,16 @@ class MovesController < ApplicationController
       format.json { head :no_content }
       format.js   { render :layout => false }
     end
+  end
+
+  private
+
+  def set_move
+    @move = Move.find_by_id(params[:id])
+  end
+
+  def move_params
+    attrs = [:move_date, :status, :user_register_id, :user_take_id, :voucher_type, :voucher_number]
+    params.require(:move).permit(attrs)
   end
 end
