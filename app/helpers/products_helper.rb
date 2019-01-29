ccea0dfd0467e11dec16279cd842ab1c529db95d
Juan Antonio Damian Vargas <juanantoniodamianv@gmail.com>
2019-01-29 12:50:02 -0300

    [Fix] Update Helper Into Products

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -17,7 +17,7 @@ module ProductsHelper
   # Devuelve una descripción del producto que incluye Número de Producto, Nombre y Tipo.
   def product_label(prod)
     return unless prod
-    "#{prod.product_number} - (#{prod.name}) - (#{prod.product_type})"
+    "#{prod.product_number} - (#{prod.name}) - (#{prod.product_type.name})"
   end
 
   module_function :product_label
