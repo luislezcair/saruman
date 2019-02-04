78b2aac2d5b14d888adb54ec5f9086ca0a6f99bd
Juan Antonio Damian Vargas <juanantoniodamianv@gmail.com>
2019-02-04 19:54:03 -0300

    Has Been Added View For Deposit->Products Inventory

---
+++ app/javascript/controllers/inventory_controller.js
@@ -0,0 +1,20 @@
+import Rails from 'rails-ujs';
+import $ from 'jquery';
+import { Controller } from 'stimulus';
+
+export default class extends Controller {
+  initialize() {
+    document.getElementById('inventories-deposit-table-container').style.display = 'none';
+    document.getElementById('inventories-table-container').style.display = 'block';
+  }
+
+  inventoryPerDeposit() {
+    if (document.getElementById('change-inventory-view').checked) {
+      document.getElementById('inventories-deposit-table-container').style.display = 'block';
+      document.getElementById('inventories-table-container').style.display = 'none';
+    } else {
+      document.getElementById('inventories-deposit-table-container').style.display = 'none';
+      document.getElementById('inventories-table-container').style.display = 'block';
+    }
+  }
+}
