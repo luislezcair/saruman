2e1cd3442d42736b96818045bf86924894f2f409
gus <gustavo.fabian.sandoval@gmail.com>
2019-02-05 12:59:30 -0300

    se agrego la columna deposito origene n movimientos

--- db/schema.rb
+++ db/schema.rb
@@ -35,9 +35,6 @@ ActiveRecord::Schema.define(version: 2019_02_05_142835) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "provider_id"
-    t.string "city"
-    t.string "country"
-    t.string "state"
     t.bigint "city_id"
     t.bigint "province_id"
     t.bigint "country_id"
