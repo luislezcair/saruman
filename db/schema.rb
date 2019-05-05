dfbe23581a9fcbd5969d00d4390720ddd7bf956a
Luis Lezcano Airaldi <luislezcair@gmail.com>
2019-05-04 21:21:28 -0300

    Creación del usuario admin inicial
    Actualización de dependencias

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_05_173140) do
+ActiveRecord::Schema.define(version: 2019_02_10_230005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,27 @@ ActiveRecord::Schema.define(version: 2019_02_05_173140) do
     t.index ["ancestry"], name: "index_accounts_on_ancestry"
   end
 
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
   create_table "addresses", force: :cascade do |t|
     t.string "street"
     t.string "house_number"
@@ -35,6 +56,9 @@ ActiveRecord::Schema.define(version: 2019_02_05_173140) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "provider_id"
+    t.string "city"
+    t.string "country"
+    t.string "state"
     t.bigint "city_id"
     t.bigint "province_id"
     t.bigint "country_id"
@@ -126,6 +150,8 @@ ActiveRecord::Schema.define(version: 2019_02_05_173140) do
     t.text "notes"
     t.string "postal_code"
     t.integer "organization_id"
+    t.date "date_of_birth"
+    t.integer "plan_service_id"
     t.index ["ucrm_id"], name: "index_clients_on_ucrm_id", unique: true
   end
 
@@ -250,8 +276,8 @@ ActiveRecord::Schema.define(version: 2019_02_05_173140) do
     t.datetime "updated_at", null: false
     t.decimal "net_amount", precision: 17, scale: 4, default: "0.0", null: false
     t.decimal "iva_amount", precision: 17, scale: 4, default: "0.0", null: false
-    t.integer "concept_id", null: false
-    t.string "account_code", null: false
+    t.integer "concept_id", default: 902253, null: false
+    t.string "account_code", default: "", null: false
   end
 
   create_table "invoices", force: :cascade do |t|
@@ -518,6 +544,18 @@ ActiveRecord::Schema.define(version: 2019_02_05_173140) do
     t.index ["name"], name: "index_transmitters_on_name", unique: true
   end
 
+  create_table "ucrm_webhooks", force: :cascade do |t|
+    t.uuid "uuid"
+    t.string "change_type"
+    t.string "entity"
+    t.integer "entity_id"
+    t.string "event_name"
+    t.string "status", default: "pending", null: false
+    t.string "error_msg"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -572,6 +610,7 @@ ActiveRecord::Schema.define(version: 2019_02_05_173140) do
     t.index ["name"], name: "index_work_types_on_name", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "addresses", "cities"
   add_foreign_key "addresses", "countries"
   add_foreign_key "addresses", "providers"
